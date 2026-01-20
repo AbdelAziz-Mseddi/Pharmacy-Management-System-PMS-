4b08cfd4d83629d21eb9ccccfe04b5235e06ab67
AbdelAziz Mseddi <msedutent@gmail.com>
2026-01-20 02:42:36 +0100

    commande and employe DAOs + connexion

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,20 @@
 package app;
 
+import java.sql.Connection;
+import connexion_sql.Connexion;
+
 public class Main {
 	public static void main(String[] args) {
+		// Test database connection
+		Connection conn = Connexion.getConnection();
 		
+		if (conn != null) {
+			System.out.println("La connexion à MySQL a réussi!");
+			
+			
+			
+		} else {
+			System.out.println("Échec de la connexion à MySQL.");
+		}
 	}
 }
