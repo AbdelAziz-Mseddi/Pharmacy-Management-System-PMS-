c799c1bfb046e4e5969b354e4423c6edd4c823d0
maha-yousfi <mahayousfi377@gmail.com>
2026-01-21 23:15:10 +0100

    Add files via upload

---
+++ src/connexion_sql/TestConnexion.java
@@ -0,0 +1,19 @@
+package connexion_sql;
+
+public class TestConnexion {
+	public static void main(String[] args) 
+	{
+        try 
+        {
+            Connexion.getConnection();
+            System.out.println("Connexion réussie !");
+        } 
+        catch (Exception e) 
+        {
+            System.out.println("Erreur de connexion !");
+            e.printStackTrace();
+        }
+    }
+
+}
+
