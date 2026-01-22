4b60b0f73af5554bf7745814e4147f2ca03820be
Yasmine-Amouri <amouriyasmine3@gmail.com>
2026-01-22 01:53:45 +0100

    Add files via upload

---
+++ src/connexion_sql/Connexion.java
@@ -0,0 +1,19 @@
+package connexion_sql;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Connexion 
+{
+	private static final String URL = "jdbc:mysql://localhost:3306/pharmacie?useSSL=false&serverTimezone=UTC";
+	private static final String USER = "root";
+	private static final String PASSWORD = "JeMangeUnePomme@2025";
+
+    public static Connection getConnection() throws SQLException 
+    {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+}
+
+
