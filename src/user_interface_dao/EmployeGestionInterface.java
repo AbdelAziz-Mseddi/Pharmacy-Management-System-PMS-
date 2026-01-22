6f04b1a6b599fa9c1d16458f0f29e08b6fd41d6b
Yasmine-Amouri <amouriyasmine3@gmail.com>
2026-01-22 02:06:11 +0100

    Add files via upload

---
+++ src/user_interface_dao/EmployeGestionInterface.java
@@ -0,0 +1,40 @@
+package user_interface_dao;
+
+import java.sql.*;
+import connexion_sql.Connexion;
+
+public class EmployeGestionInterface {
+
+    public static int creerEmploye(String nom, String prenom, String tel, String mail, String mdp, String privilege) {
+        String sqlInsert = "INSERT INTO Employe (nom, prenom, tel, mail, mdp, privilege) VALUES (?, ?, ?, ?, ?, ?)";
+
+        try (Connection cnx = Connexion.getConnection();
+             PreparedStatement ps = cnx.prepareStatement(sqlInsert)) {
+
+            ps.setString(1, nom);
+            ps.setString(2, prenom);
+            ps.setInt(3, Integer.parseInt(tel));
+            ps.setString(4, mail);
+            ps.setString(5, mdp);
+            ps.setString(6, privilege.toLowerCase());
+
+            int rows = ps.executeUpdate();
+
+            if (rows > 0) {
+                String sqlSelect = "SELECT idEmploye FROM Employe WHERE tel = ?";
+                try (PreparedStatement ps2 = cnx.prepareStatement(sqlSelect)) {
+                    ps2.setInt(1, Integer.parseInt(tel));
+                    ResultSet rs = ps2.executeQuery();
+                    if (rs.next()) {
+                        return rs.getInt("idEmploye");
+                    }
+                }
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return -1;
+    }
+}
---
+++ src/user_interface_dao/EmployeInterface.java
@@ -0,0 +1,49 @@
+package user_interface_dao;
+
+import java.sql.*;
+import classes_principales.Employe;
+import connexion_sql.Connexion;
+
+public class EmployeInterface {
+
+
+    public static Employe login(int idEmploye, String motDePasse) {
+
+        Connection cnx = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try {
+            cnx = Connexion.getConnection();
+
+            String sql = "SELECT * FROM Employe WHERE idEmploye = ? AND mdp = ?";
+            ps = cnx.prepareStatement(sql);
+            ps.setInt(1, idEmploye); 
+            ps.setString(2, motDePasse); 
+
+            rs = ps.executeQuery();
+
+            if (rs.next()) {
+
+                int id = rs.getInt("idEmploye");
+                String nom = rs.getString("nom");
+                String prenom = rs.getString("prenom");
+                int tel = rs.getInt("tel");
+                String email = rs.getString("mail");
+                String mdp = rs.getString("mdp");
+                String privilege = rs.getString("privilege");
+
+                return new Employe(id, nom, prenom, tel, email, mdp, privilege);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try { if (rs != null) rs.close(); } catch (Exception e) {}
+            try { if (ps != null) ps.close(); } catch (Exception e) {}
+            try { if (cnx != null) cnx.close(); } catch (Exception e) {}
+        }
+
+        return null;
+    }
+}
